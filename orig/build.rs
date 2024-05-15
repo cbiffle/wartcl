fe5f81582fb404f7f81bbc534d465d9711f89dbc
Cliff L. Biffle <code@cliffle.com>
2024-05-14 21:08:26 -0700

    Ensure partcl gets rebuilt on changes.

--- orig/build.rs
+++ orig/build.rs
@@ -1,4 +1,8 @@
 fn main() {
+    // TODO: it is not clear why I need to do this by hand, one would think the
+    // cc crate would be doing it.
+    println!("cargo::rerun-if-changed=src/tcl.c");
+
     cc::Build::new()
         .file("src/tcl.c")
         .compile("partcl");
